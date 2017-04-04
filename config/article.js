718ec4465890d4b955541028879ac7250f96df26
David Blood <davidbjourno@users.noreply.github.com>
2017-04-04 07:51:41 +0100

    Update publication date

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date('2017-04-02T18:00:00Z'),
+  publishedDate: new Date('2017-04-04T05:00:00Z'),
 
   headline: 'Women vastly outnumbered by men as they climb the corporate ranks',
 
